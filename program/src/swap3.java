6214ab65ec6435cbbf0cd4f47c5bc966ec63e619
Raja <Raja@Raja_lap>
2018-11-28 18:45:10 +0530

    second push

---
+++ program/src/swap3.java
@@ -0,0 +1,15 @@
+class swap3 {
+
+	public static void main(String[] args) {
+	int a=7,b=4,c=3,d;
+	System.out.println("Before Swapping");
+	System.out.println(a+" "+b+" "+c);
+	d=a;
+	a=b;
+	b=c;
+	c=d;
+	System.out.println("After Swapping");
+	System.out.println(a+" "+b+" "+c);
+	}
+
+}
